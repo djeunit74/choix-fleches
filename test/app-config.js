dd0d4f5812d7b19fb4613be9de3317788da962dc
djeunit74 <68806290+djeunit74@users.noreply.github.com>
2026-08-18 17:10:16 +0200

    Refactor: centralise la version TEST

--- test/app-config.js
+++ test/app-config.js
@@ -1,6 +1,6 @@
-/* Configuration centrale Assistant Archer TEST. Aucun comportement métier ici. */
+/* Configuration centrale Assistant Archer TEST. Aucun comportement metier ici. */
 window.AssistantArcherConfig = Object.freeze({
-  version: '2026.08.18-refactor.1',
+  version: '2026.08.18-refactor.2',
   channel: 'test',
   historyLimit: 5,
   principles: Object.freeze({
