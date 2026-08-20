a9f34facd716fe82c61ecbd4185cc8a9c124d058
djeunit74 <68806290+djeunit74@users.noreply.github.com>
2026-08-20 12:14:28 +0200

    TEST: passe la version en v44

--- test/app-config.js
+++ test/app-config.js
@@ -1,6 +1,6 @@
 /* Configuration centrale Assistant Archer TEST. Aucun comportement metier ici. */
 window.AssistantArcherConfig = Object.freeze({
-  version: '2026.08.20-v43',
+  version: '2026.08.20-v44',
   channel: 'test',
   historyLimit: 5,
   principles: Object.freeze({
