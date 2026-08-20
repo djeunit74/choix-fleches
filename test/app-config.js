5c1a89e4b6b3e91d23003048bede41143062cad2
djeunit74 <68806290+djeunit74@users.noreply.github.com>
2026-08-20 08:18:48 +0200

    TEST: passe la version en v41 apres simplification du cache

--- test/app-config.js
+++ test/app-config.js
@@ -1,6 +1,6 @@
 /* Configuration centrale Assistant Archer TEST. Aucun comportement metier ici. */
 window.AssistantArcherConfig = Object.freeze({
-  version: '2026.08.20-v40',
+  version: '2026.08.20-v41',
   channel: 'test',
   historyLimit: 5,
   principles: Object.freeze({
