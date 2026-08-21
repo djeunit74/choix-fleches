205a9f706c59264353b8b9a9edf9383f6d3cf9bd
djeunit74 <68806290+djeunit74@users.noreply.github.com>
2026-08-21 11:53:10 +0200

    TEST v57: synchronise la version centrale

--- test/app-config.js
+++ test/app-config.js
@@ -1,6 +1,6 @@
 /* Configuration centrale Assistant Archer TEST. Aucun comportement metier ici. */
 window.AssistantArcherConfig = Object.freeze({
-  version: '2026.08.21-v56',
+  version: '2026.08.21-v57',
   channel: 'test',
   historyLimit: 5,
   principles: Object.freeze({
