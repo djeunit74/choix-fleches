2da28558a7f3ac1816b848ae13d0045bc8bd7dfe
djeunit74 <68806290+djeunit74@users.noreply.github.com>
2026-08-21 10:06:10 +0200

    TEST v54: versionne le catalogue de pointes

--- test/app-config.js
+++ test/app-config.js
@@ -1,6 +1,6 @@
 /* Configuration centrale Assistant Archer TEST. Aucun comportement metier ici. */
 window.AssistantArcherConfig = Object.freeze({
-  version: '2026.08.21-v53',
+  version: '2026.08.21-v54',
   channel: 'test',
   historyLimit: 5,
   principles: Object.freeze({
