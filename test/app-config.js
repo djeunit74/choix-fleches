0046a8450b75712ee6c3793f516df188b999314e
djeunit74 <68806290+djeunit74@users.noreply.github.com>
2026-08-21 18:22:21 +0200

    TEST v60: versionne les masses arriere Skylon

--- test/app-config.js
+++ test/app-config.js
@@ -1,6 +1,6 @@
 /* Configuration centrale Assistant Archer TEST. Aucun comportement metier ici. */
 window.AssistantArcherConfig = Object.freeze({
-  version: '2026.08.21-v59',
+  version: '2026.08.21-v60',
   channel: 'test',
   historyLimit: 5,
   principles: Object.freeze({
