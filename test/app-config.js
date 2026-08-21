6e22c8e8fb371196681035e093acf1122ff58dcd
djeunit74 <68806290+djeunit74@users.noreply.github.com>
2026-08-21 11:01:42 +0200

    TEST v56: passe la version centrale

--- test/app-config.js
+++ test/app-config.js
@@ -1,6 +1,6 @@
 /* Configuration centrale Assistant Archer TEST. Aucun comportement metier ici. */
 window.AssistantArcherConfig = Object.freeze({
-  version: '2026.08.21-v55',
+  version: '2026.08.21-v56',
   channel: 'test',
   historyLimit: 5,
   principles: Object.freeze({
